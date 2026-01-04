e6e0a5ed581bb044644017e802283bd17749c86a
veera <ramanavalar2001@gmail.com>
2026-01-04 19:28:22 +0530

    Add runtime

--- config/settings.py
+++ config/settings.py
@@ -78,11 +78,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ["MYSQLDATABASE"],
+        "USER": os.environ["MYSQLUSER"],
+        "PASSWORD": os.environ["MYSQLPASSWORD"],
+        "HOST": os.environ["MYSQLHOST"],
+        "PORT": os.environ["MYSQLPORT"],
     }
 }
+
 
 
 # Password validation
